12262cfe73f6d71a871d6e7111b16fcb1e60d888
alamia442 <61982500+alamia442@users.noreply.github.com>
2022-05-16 18:39:07 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ ls -lah
 
 sleep 45
 
-cd /usr/src && rm -rf * && git clone https://$TOKEN@github.com/$NAME/$REPO .
+cd /usr/src && rm -rf * && ls -lah && git clone https://$TOKEN@github.com/$NAME/$REPO .
 cd /usr/src && chmod +x start.sh && bash start.sh
