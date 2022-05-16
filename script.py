379adfe4a2537442fa8416a6f46821fb3854bca1
alamia442 <61982500+alamia442@users.noreply.github.com>
2022-05-16 10:19:30 +0700

    Update script.py

--- script.py
+++ script.py
@@ -1,16 +1,16 @@
+import os
+
 from flask import Flask
-from datetime import datetime
+
 app = Flask(__name__)
 
-@app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+@app.route("/")
+def hello_world():
+    app_name = os.environ.get("APP_NAME", "My App")
+    return f"<h1>Hello from {app_name}!<h1>"
 
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+
+if __name__ == "__main__":
+    hport = int(os.environ.get('PORT', 17995))
+    app.run(debug=True, port={hport})
