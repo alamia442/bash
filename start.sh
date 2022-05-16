acd5cda704940482f655829fc5699fda51ed10a7
alamia442 <61982500+alamia442@users.noreply.github.com>
2022-05-16 18:54:16 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,9 @@
 
 curl -s ipinfo.io
 
-sleep 45
-
 cd /usr/src && rm -rf * && rm -rf .git && git clone https://$TOKEN@github.com/$NAME/$REPO .
+gunicorn web.wserver:app --bind 0.0.0.0:$PORT
+
+sleep 5m
+
 cd /usr/src && chmod +x start.sh && bash start.sh
