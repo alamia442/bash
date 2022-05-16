daacce067fd5479e960589d41c24be9a9d4a8473
alamia442 <61982500+alamia442@users.noreply.github.com>
2022-05-16 10:11:47 +0700

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,16 @@
-import time
+from flask import Flask
+from datetime import datetime
+app = Flask(__name__)
 
-while True:
-	print("I am working!")
-	time.sleep(2)
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+    <img src="http://loremflickr.com/600/400" />
+    """.format(time=the_time)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
