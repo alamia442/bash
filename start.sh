0ed38aa30833b1691249cf64546401165ac976c3
alamia442 <61982500+alamia442@users.noreply.github.com>
2022-05-16 19:40:02 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-set -e
+
+sleep-with-countdown() {
+  secs=$1
+  while [ $secs -gt 0 ]; do
+    printf "\rsleep: $secs\033[0K"
+    sleep 1
+    : $((secs--))
+  done
+  printf "\n"
+}
 
 curl -s ipinfo.io
 
@@ -10,11 +19,6 @@ sleep 45
 cd /usr/src && rm -rf * && rm -rf .git && git clone https://$TOKEN@github.com/$NAME/$REPO .
 gunicorn web.wserver:app --bind 0.0.0.0:$PORT --daemon &
 
-secs=$((5 * 60))
-while [ $secs -gt 0 ]; do
-   echo -ne "$secs\033[0K\r"
-   sleep 1
-   : $((secs--))
-done
+sleep-with-countdown(5)
 
 cd /usr/src && chmod +x start.sh && bash start.sh
